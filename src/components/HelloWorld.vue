b833d0d5db5ad0d590384456a9d6375ac2599519
ZHAISHENKING <18700790825@163.com>
2018-09-08 10:41:08 +0800

    脚手架

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,49 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<div style="width: 100%; background: #fff; padding: 8px;">
+  <mu-flex class="flex-wrapper" justify-content="start">
+    <mu-flex class="flex-demo" justify-content="center" >left</mu-flex>
+  </mu-flex>
+  <mu-flex class="flex-wrapper" justify-content="center">
+    <mu-flex class="flex-demo" justify-content="center" >center</mu-flex>
+  </mu-flex>
+  <mu-flex class="flex-wrapper" justify-content="end">
+    <mu-flex class="flex-demo" justify-content="center" >right</mu-flex>
+  </mu-flex>
+  <mu-flex class="flex-wrapper" align-items="center">
+    <mu-flex class="flex-demo" justify-content="center" fill>fill</mu-flex>
+    <mu-flex class="flex-demo" justify-content="center" fill>fill</mu-flex>
+    <mu-flex class="flex-demo" justify-content="center" fill>fill</mu-flex>
+  </mu-flex>
+</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        }
+    }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  .flex-wrapper {
+    width: 100%;
+    height: 56px;
+    margin-top: 8px;
+  }
+  .flex-demo {
+    width: 200px;
+    height: 32px;
+    background-color: #e0e0e0;
+    text-align: center;
+    line-height: 32px;
+    margin-left: 8px;
+  }
+  .flex-wrapper:first-child {
+    margin-top: 0;
+  }
+  .flex-demo:first-child {
+    margin-left: 0;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css';
+// import theme from 'muse-ui/lib/theme';
+//
+// theme.use('dark');
+Vue.use(MuseUI);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
+  store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
