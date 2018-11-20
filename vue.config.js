37333e09203452c903190b67f2a38ec5626811e8
ZHAISHENKING <18700790825@163.com>
2018-11-20 14:50:57 +0800

    静态资源配置

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,16 @@
+const path = require('path');
+function resolve (dir) {
+    return path.join(__dirname, dir)
+}
+
+
 module.exports = {
-    configureWebpack: {
-        // other webpack options to merge in ...
-    },
+    baseUrl: '/',
+    // 打包项目输出目录
+    outputDir: "dist",
+    // 静态资源存放目录
+    assetsDir: "static",
+    lintOnSave: true,
     // devServer Options don't belong into `configureWebpack`
     devServer: {
         host: '0.0.0.0',
